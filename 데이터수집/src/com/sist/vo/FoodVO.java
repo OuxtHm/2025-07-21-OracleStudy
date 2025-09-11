bf563663d969d88d24421a1bd8a48d2aad913bd9
OuxtHm <121115592+OuxtHm@users.noreply.github.com>
2025-09-11 17:34:07 +0900

    day9

--- 데이터수집/src/com/sist/vo/FoodVO.java
+++ 데이터수집/src/com/sist/vo/FoodVO.java
@@ -6,6 +6,6 @@ import lombok.Data;
 public class FoodVO {
 	private int fno, hit;
 	private double score;
-	private String name, type, phone, address, theme, parking, poster, images, content, price;
+	private String name, type, phone, address, theme, parking, poster, images, content, price, time;
 	
 }
